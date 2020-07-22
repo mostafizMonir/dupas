9d4d09f3cdead32fe156b86f1c629d5efddfdf15
Monir <monir.ndc.itdu@gmail.com>
2020-07-22 18:43:27 +0600

    send mail

--- spl/email/mpdf/index2.php
+++ spl/email/mpdf/index2.php
@@ -2,12 +2,15 @@
     <input type="submit" name="test" id="test" value="RUN" /><br/>
 </form>
 
+
 <?php
 
 function testfun()
 {
     require_once '../../../spl/lib/vendor/autoload.php';
+$sub='history';
 
+    $attachment = new Swift_Attachment('', '../../email/files/filename.pdf', 'application/pdf');
 
     $transport = (new Swift_SmtpTransport('smtp.gmail.com',587,'tls'))
         ->setUsername('monir.ndc.itdu@gmail.com')
@@ -24,11 +27,248 @@ function testfun()
         ->setFrom(['monir.ndc.itdu@gmail.com'])
 
         // Include several To addresses
-        ->setTo(['bit0211@iit.du.ac.bd']);
 
-  /*  if($mailer->send($message)){
+        ->setTo(['bit0211@iit.du.ac.bd'])
+        ->attach($attachment)
+    ->setBody('
+    
+<html>
+<head>
+  
+</head>
+
+<body>
+
+<div class=\\"contain\\">
+
+<style scoped>
+     .contain{ border-style: solid;
+               padding-left:125px;
+               padding-top:100px;
+           
+      }
+ </style>
+<div class=\\"row\\">
+<style scoped>
+    
+  
+
+ 
+
+/* Clear floats after the columns */
+.row:after {
+    content: \\"\\";
+      display: table;
+      clear: both;
+}
+      }
+ </style>
+
+  <div class=\\"column address\\" >
+  <style scoped>
+ .column {
+        
+        float: left;
+
+        height: 200px; /* Should be removed. Only for demonstration */
+}
+
+     .address{
+
+width: 35%;
+
+
+}
+ </style>
+
+  <strong>পরীক্ষা নিয়ন্ত্রকের অফিস</strong><br>
+        <strong>ঢাকা বিশ্ববিদ্যালয়</strong><br>
+        ঢাকা-১০০০,বাংলাদেশ।<br>
+        ফোনঃ(অফিস)৮৬১৩২৮০<br>
+        ৯৬৬১৯০০-৫৯/৪০৭৫<br>
+        (বাসা)৯৬৬৬১৩৫<br>
+        ফ্যাক্সঃ৮৮০-২-৯৬৬৭২২২<br>
+        Email:examcontroller@du.ac.bd
+  </div>
+  <div class=\\"column image\\">
+
+  <style scoped>
+ .column {
+        
+        float: left;
+
+        height: 200px; /* Should be removed. Only for demonstration */
+}
+
+     .image{
+
+width: 35%;
+padding-left:50px;
+}
+ </style>
+
+  <p> <img src=\\" ../mpdf / DhakaUniversity . jpg\\" width=\\"70\\" height=\\"90\\"></p>
+  </div>
+  
+</div>
+
+
+
+<div class=\\"row\\">
+<style scoped>
+    
+  
+
+ 
+
+/* Clear floats after the columns */
+.row:after {
+    content: \\"\\";
+      display: table;
+      clear: both;
+}
+      }
+ </style>
+
+  <div class=\\"column p1\\" >
+  <style scoped>
+ .column {
+        
+        float: left;
+
+       /* Should be removed. Only for demonstration */
+}
+
+     .p1{
+
+width: 35%;
+
+padding-top:-20px;
+height:30px;
+}
+ </style>
+
+ <p> <p>মেমো নংঃ $memo শা-৫/প</p></p>
+  </div>
+
+  <div class=\\"column p2\\">
+
+<style scoped>
+.column {
+      
+      float: left;
+
+     /* Should be removed. Only for demonstration */
+}
+
+   .p2{
+
+width: 30%;
+
+padding-left:270px;
+padding-top:20px;
+height: 50px;
+}
+</style>
+
+তারিখঃ  $new
+</div>
+
+
+
+</div>
+
+<div class=\\"main1\\">
+
+চেয়ারম্যান,<br>
+
+ $dept <br>
+ঢাকা বিশ্ববিদ্যালয়।<br>
+
+</div>
+
+<div class=\\"main2\\">
+                   <p> বিষয়ঃ- সমন্বিত  কোর্স  পদ্ধতিতে  সম্মান  পরীক্ষার  পরীক্ষকদের  প্যানেল  ও  পরীক্ষা  কমিটি  গঠন।<p><br>
+                        জনাব,<br>
+                </div>
+
+<div class=\\"main3\\">
+
+<style scoped>
+.main3
+{
+text-indent:60px;
+word-spacing: 8px;
+
+}
+
+</style>
+
+$sub  বিষয়ে   সমন্বিত    কোর্স    পদ্ধতির   $y সনের  $yr<br>
+বর্ষ  সম্মান  ও  সহায়ক  পরীক্ষকদের  প্যানেল
+বিভাগীয়    কমিটি    অব    কোর্সেস    এর    সুপারিশ    অনুযায়ী <br>   গঠন    করিয়া    একাডেমিক    কাউন্সিলের
+অনুমোদনের    জন্য    সংশ্লিষ্ট    অনুষদের    মাধ্যমে    এবং    পরীক্ষা <br>   কমিটি    বিভাগীয়    একাডেমিক    কমিটির
+ সুপারিশ    অনুযায়ী    গঠন    করিয়া  $y1  ইং<br>   তারিখের    মধ্যে    সরাসরি    অত্র     অফিসে
+ পাঠানোর    জন্য    আপনাকে    অনুরোধ  করিতেছি।<br>
+
+</div>
+
+                <div class=\\"h\\">
+
+<style scoped>
+.h
+{
+   margin-left:250px ;
+   padding-top: 10px;
+   padding-bottom:-20px;
+   width: 30%;
+   
+   height:60px;
+ 
+   padding-left: 300px;
+}
+
+       </style>
+
+<p>আপনার বিশ্বস্ত</p>
+
+
+</div>
+
+
+<div class=\\"g\\">
+
+<style scoped>
+.g
+{width: 25%;
+padding-left:200px;
+margin-left:300px;
+text-indent:-4px;
+height: 200px; 
+}
+
+       </style>
+
+<img src=\\"signature . jpg\\" width=\\"180\\" height=\\"50\\">
+     
+<p style=\\"margin - left:14 %;\\">পরীক্ষা নিয়ন্ত্রকের পক্ষে<br>
+     পরীক্ষা উপ-নিয়ন্ত্রক<br>
+     ঢাকা বিশ্ববিদ্যালয়।<br>
+     </p>
+
+
+</div>
+</div>
+  
+
+</body>
+</html>
+    ', 'text/html');
+
+
+    if($mailer->send($message)){
         echo "mail send";
-    }*/
+    }
 
 
 }
@@ -56,50 +296,50 @@ if(array_key_exists('test',$_POST)){
 </form>
 </body>
 </html>
-
 <?php
 
 class BanglaConverter {
     public static $bn = array("১", "২", "৩", "৪", "৫", "৬", "৭", "৮", "৯", "০");
     public static $en = array("1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
-    
+
     public static function bn2en($number) {
         return str_replace(self::$bn, self::$en, $number);
     }
-    
+
     public static function en2bn($number) {
         return str_replace(self::$en, self::$bn, $number);
     }
- }
+}
 
 
-                      $orgDate = $_POST["birthday"];  
-                      $newDate = date("d-m-Y", strtotime($orgDate));  
-                      $new= BanglaConverter::en2bn($newDate);
+$orgDate = $_POST["birthday"];
+$newDate = date("d-m-Y", strtotime($orgDate));
+$new= BanglaConverter::en2bn($newDate);
 
 
-                      $memo= BanglaConverter::en2bn( $_POST["memo"]);
+$memo= BanglaConverter::en2bn( $_POST["memo"]);
 
-                      $dept=$_POST["dept"];
+$dept=$_POST["dept"];
 
-                      $sub=$_POST["sub"];
+$sub=$_POST["sub"];
+$subject=$sub;
 
-                      $y=BanglaConverter::en2bn($_POST["year"]);
+$y=BanglaConverter::en2bn($_POST["year"]);
 
-                      $yr=$_POST["yr"];
+$yr=$_POST["yr"];
 
-                      $orgDate = $_POST["birthday1"];  
-                      $newDate1 = date("d-m-Y", strtotime($orgDate));  
-                      $y1= BanglaConverter::en2bn($newDate1);
+$orgDate = $_POST["birthday1"];
+$newDate1 = date("d-m-Y", strtotime($orgDate));
+$y1= BanglaConverter::en2bn($newDate1);
 
 
 
-                      $conn=mysqli_connect("localhost","root","monmosndc","letter");
-                      $sql="INSERT INTO letter_data(MemoNo,SendingDate,
+$conn=mysqli_connect("localhost","root","monmosndc","letter");
+$sql="INSERT INTO letter_data(MemoNo,SendingDate,
                             Department,Year,Subject,StudyYear,ResponseDate,Type)
                             VALUES ('$memo','$new','$dept','$y','$sub','$yr','$y1','B')";
-                     $result=mysqli_query($conn,$sql);
-                     mysqli_close($conn);
+$result=mysqli_query($conn,$sql);
+mysqli_close($conn);
 
 
 
@@ -349,7 +589,8 @@ height: 200px;
 
 
 
-$html = ob_get_contents();
+global $html;
+$html =ob_get_contents();
 ob_end_clean();
 
 $mpdf->WriteHTML($html);
